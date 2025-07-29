55281c2bbddc55715b255aac8b628b35011832f0
Apari72 <ahmedakifapari1401@gmail.com>
2025-07-29 18:18:32 +0300

    collaborator eklemdi

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False  # Türkçe karakter sorununu önler
 
-CORS(app)  # Frontend'in API'ye erişmesini sağlar
+CORS(app)  # Frontend'in API'ye erişmesini sağlar alpor
 
 # Test endpoint'i
 @app.route("/hello", methods=["GET"])
